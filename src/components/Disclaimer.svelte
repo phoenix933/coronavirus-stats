95b16db259fac090f0ccba32c57ebb9ac135b7cb
crashdev226 <midashand44@gmail.com>
2020-03-23 21:06:39 +0200

    chore: add Discailmer component

---
+++ src/components/Disclaimer.svelte
@@ -0,0 +1,25 @@
+<script>
+    const HOME_URL = 'https://koronavirus-bg.web.app/';
+    const COVID_API_URL = 'https://github.com/javieraviles/covidAPI';
+    const COVID_API_OWNER_URL = 'https://github.com/javieraviles';
+    const WORLDOMETER_URL = 'https://www.worldometers.info/coronavirus/';
+    const WORLDOMETER_CONTACT_URL = 'https://www.worldometers.info/contact/';
+
+</script>
+
+<p>
+    * Този уебсайт 
+    <a href={HOME_URL}>(koronavirus-bg)</a>
+    използва данни от уебсайта
+    <a href={WORLDOMETER_URL} target="_blank">Worldometer</a>, 
+    посредством публичната уеб услуга 
+    <a href={COVID_API_URL} target="_blank">Covid API</a>.
+    Собственикът на този уебсайт 
+    <a href={HOME_URL}>(koronavirus-bg)</a>
+    не носи отговорност за коректността на данните в него.
+    При неверни данни, моля свържете се със 
+    <a href={COVID_API_OWNER_URL} target="_blank">собственика</a> на уеб услугата
+    <a href={COVID_API_URL} target="_blank">Covid API</a> - 
+    <a href={COVID_API_OWNER_URL} target="_blank">Javier Aviles</a> или с 
+    <a href={WORLDOMETER_CONTACT_URL} target="_blank">Worldometer</a>.
+</p>
