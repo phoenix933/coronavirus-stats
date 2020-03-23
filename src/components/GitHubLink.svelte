0c05e1319e2d7c61ff0f896d96b8aff4083bf66c
crashdev226 <midashand44@gmail.com>
2020-03-23 21:10:51 +0200

    chore: add GItHubLink component

---
+++ src/components/GitHubLink.svelte
@@ -0,0 +1,24 @@
+<p class="github">
+    <a href="https://github.com/yuliankarapetkov/coronavirus-stats" target="_blank">
+        <img src="/images/github.png" alt="Github logo" /> Source code
+    </a>
+</p>
+
+<style>
+    .github a {
+		display: flex;
+		align-items: center;
+		margin-bottom: 2rem;
+	}
+
+	.github a img {
+		height: 1.5rem;
+		margin-right: 0.5rem;
+    }
+    
+    @media screen and (max-width: 768px) {
+		.github {
+			margin-bottom: 2rem;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -28,6 +28,7 @@
 <script>
 	import Breakdown from '../components/Breakdown.svelte'
 	import Disclaimer from '../components/Disclaimer.svelte';
+	import GitHubLink from '../components/GitHubLink.svelte';
 	import Table from '../components/Table.svelte'
 
 	export let total;
@@ -67,11 +68,7 @@
 
 	<Disclaimer />
 
-	<p class="github">
-		<a href="https://github.com/yuliankarapetkov/coronavirus-stats" target="_blank">
-			<img src="/images/github.png" alt="Github logo" /> Source code
-		</a>
-	</p>
+	<GitHubLink />
 
 	{#if total}
 		<Breakdown title="🌍 В целия свят" {...total} />
@@ -91,24 +88,9 @@
 		margin: 30px
 	}
 
-	.github a {
-		display: flex;
-		align-items: center;
-		margin-bottom: 2rem;
-	}
-
-	.github a img {
-		height: 1.5rem;
-		margin-right: 0.5rem;
-	}
-
 	@media screen and (max-width: 768px) {
 		.container {
 			margin: 0;
-		}
-
-		.github {
-			margin-bottom: 2rem;
 		}
 	}
 </style>
