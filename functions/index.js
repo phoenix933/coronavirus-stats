fe22196cae5a3c949056f93f80ef0db04dae213c
crashdev226 <midashand44@gmail.com>
2020-04-22 16:07:38 +0300

    feat: add text compression

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const compression = require('compression');
 
 // We have to import the built version of the server middleware.
 const { sapper } = require('./__sapper__/build/server/server');
 
-const app = express().use(sapper.middleware());
+const app = express().use(
+    compression({ threshold: 0 }), // compress everything
+    sapper.middleware()
+);
 
 exports.ssr = functions.https.onRequest(app);
